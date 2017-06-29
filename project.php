5e0da6936d93e25f27f28b871b630a0d79a4fab2
Pasit <enqueteplu@gmail.com>
2017-06-29 23:14:33 +0700

    new page name project.php find people and catagory

---
+++ project.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Project Student</title>
+    <!-- css -->
+    <link rel="stylesheet" href="stylesheet/css/style_port.css">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap-theme.min.css">
+    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web:400,600" rel="stylesheet">
+  </head>
+  <body>
+    <section class="container-fluid">
+      <section class="row">
+        <nav class="col-sm-12 wrap_nav_top">
+          <article class="col-xs-4 box_nav_top_l">
+            <img src="image_web/logo_ultraline.png" alt="Ultraline11.3_logo">
+          </article>
+          <article class="col-xs-8 box_nav_top_r">
+            <img src="image_web/logo_ict.png" alt="ictsu_logo">
+          </article>
+          <hr>
+        </nav>
+      </section> <!-- row -->
+
+      <!-- footer -->
+      <footer class="row">
+        <section class="wrap_footer">
+          <article class="col-xs-12 wrap_r_footer">
+            <article class="col-xs-3 wrap_footer_l">
+              <button type="button" name="button"><img src="image_web/btn_hamberger.png"></button>
+            </article> <!-- wrap_footer_l -->
+            <article class="col-xs-9 wrap_footer_r">
+              <p>HOME > CATEGORY > APPLICATION IOS</p>
+              <article class="wrap_topic_footer">
+                <span class="topic_footer">Application IOS</span>
+              </article>
+              <article class="wrap_user_footer">
+                <span class="user_footer">12 CREATOR</span>
+              </article>
+
+            </article>
+          </article>
+        </section>
+      </footer>
+    </section> <!-- container-fluid -->
+
+
+  </body>
+</html>
