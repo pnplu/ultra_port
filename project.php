77a50bb9204304056b88b26910a8cfc9550e2a11
Pasit <enqueteplu@gmail.com>
2017-06-30 04:16:03 +0700

    add meta viewport in project.php

--- project.php
+++ project.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width= device-width, initial-scale=1.0">
     <title>Project Student</title>
     <!-- css -->
     <link rel="stylesheet" href="stylesheet/css/style_port.css">
