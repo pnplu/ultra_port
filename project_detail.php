6b9fc24a557bdbc0f4f6a70cc25a6783a70eace8
Pasit <enqueteplu@gmail.com>
2017-07-01 00:02:02 +0700

    test project_detail unsuccess

---
+++ project_detail.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width= device-width, initial-scale=1.0">
+    <title>Project Detail</title>
+    <!-- css -->
+    <link rel="stylesheet" href="stylesheet/css/style_project_detail.css">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap-theme.min.css">
+    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web:400,600" rel="stylesheet">
+  </head>
+  <body>
+    <section class="container-fluid">
+      <!-- nav -->
+      <section class="row">
+        <nav class="col-sm-12 wrap_nav_top">
+          <article class="col-xs-4 col-sm-2 box_nav_top_l">
+            <img src="image_web/logo_ultraline.png" alt="Ultraline11.3_logo">
+          </article>
+          <article class="col-sm-7 wrap_menu_top_text tablet">
+            <ul>
+              <li>CREATOR</li>
+              <li>THANK YOU</li>
+              <li>CONTACT</li>
+            </ul>
+          </article>
+          <article class="col-xs-8 col-sm-3 box_nav_top_r">
+            <img src="image_web/logo_ict.png" alt="ictsu_logo">
+          </article>
+          <hr>
+        </nav> <!-- nav -->
+      </section>
+      <!-- banner -->
+      <section class="row">
+        <article class="col-xs-12 wrap_banner">
+
+        </article>
+      </section> <!-- banner -->
+      <!-- concept -->
+      <section class="row">
+        <section class="col-xs-12 wrap_concept">
+          <h3>CONCEPT</h3>
+          <p>
+            	Travelmate แอปพลิเคชันส่งเสริมกิจกรรมและการท่องเที่ยวสำหรับครอบครัวและเด็ก ที่จะคอยเป็นคู่หูให้คำแนะนำครอบครัวที่ต้องการพาลูกๆออกไปทำกิจกรรมและท่องเที่ยวร่วมกัน ดังนั้น คุณจึงไม่ต้องกังวลเกี่ยวกับการเดินทางเลย เพราะคุณมี "Travelmate คู่หูการท่องเที่ยวสำหรับครอบครัว"
+          </p>
+        </section>
+      </section> <!-- concept -->
+      <!-- design process -->
+      <section class="row">
+        <article class="col-xs-12 wrap_design_process">
+          <h3>DESIGN PROCESS</h3>
+          <article class="row wrap_dp_user">
+            <ul class="row">
+              <li class="col-xs-5 dp_img_user_a">
+
+              </li>
+              <li class="col-xs-5 dp_img_user_b">
+
+              </li>
+              <li class="col-xs-5 dp_img_user_b">
+
+              </li>
+            </ul>
+          </article>
+        </article>
+      </section><!-- design process -->
+      <!-- function -->
+      <section class="row">
+        <section class="col-xs-12 wrap_function">
+          <h3>FUNCTION</h3>
+          <article class="col-xs-12 mockup_nb">
+
+          </article>
+          <article class="col-xs-offset-1 col-xs-10 func_a func_active">
+            <article class="row col-xs-4 func_number">
+              <h4>01</h4>
+            </article>
+            <article class="col-xs-9 func_discription">
+              <h4>GENERATE PDF</h4>
+              <p>ฟังก์ชั่นช่วยในการบันทึกภาพการเดินทาง และ ช่วยในการวางแผนการเดินทาง</p>
+            </article>
+          </article>
+        </section>
+      </section> <!-- function -->
+
+      <!-- footer -->
+      <footer class="row">
+        <section class="wrap_footer">
+          <article class="col-xs-12 wrap_r_footer">
+            <article class="col-xs-3 col-sm-2 col-md-1 wrap_footer_l">
+              <button type="button" name="button"><img src="image_web/btn_hamberger.png"></button>
+            </article> <!-- wrap_footer_l -->
+            <article class="col-xs-9 col-sm-10 col-md-11 wrap_footer_r">
+              <p>เว็บแอปพลิเคชันเพื่อส่งเสริมการทำแฟ้มสะสมผลงานให้มีประสิทธิภาพ ด้วยเทคโนโลยีเอฟเอฟเอ็มเป็ก กรณีศึกษาทำงานร่วมกับเว็บไซต์ บีแฮนซ์และดริบเบิ้ล </p>
+            </article> <!-- wrap_footer_r -->
+          </article> <!-- wrap_r_footer -->
+        </section> <!-- wrap_footer -->
+      </footer>
+    </section> <!-- container-fluid -->
+  </body>
+</html>
