42acc0cb851d551e183faffeb781e0500f28fbb9
ekkrit13 <ekkrit1993@gmail.com>
2017-07-03 14:57:16 +0700

    first look menu

---
+++ browse.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width= device-width, initial-scale=1.0">
+    <title>Project Student</title>
+    <!-- css -->
+    <link rel="stylesheet" href="stylesheet/css/style_port.css">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap-theme.min.css">
+    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web:400,600" rel="stylesheet">
+  </head>
+  <body>
+    <section class="container-fluid">
+      <section class="row">
+        <nav class="col-sm-12 wrap_nav_top">
+          <article class="col-xs-4 col-sm-2 box_nav_top_l">
+            <img src="image_web/logo_ultraline.png" alt="Ultraline11.3_logo">
+          </article>
+
+
+
+        </nav> <!-- nav -->
+
+        <section class="col-xs-12 col-sm-11 wrap_people">
+          <article class="col-xs-6 col-sm-4 col-md-3 wrap_box_people">
+            <!-- <article class="row"> -->
+              <article class="image_people">
+                <img src="image_people/01.jpg" alt="Ultraline 11.3">
+                  <button type="button">VIEW PROJECT</button>
+              </article>
+            <!-- </article> -->
+            <!-- <article class="row"> -->
+              <article class="discription_people">
+                <p class="topic_user">Space in galaxy</p>
+                <p class="name_user">by May Valentine</p>
+              </article> <!-- discription_people -->
+            <!-- </article> -->
+
+          </article> <!-- wrap_box_people 1 -->
+          <article class="col-xs-6 col-sm-4 col-md-3 wrap_box_people">
+            <!-- <article class="row"> -->
+              <article class="image_people">
+                <img src="image_people/02.jpg" alt="Ultraline 11.3">
+                  <button type="button">VIEW PROJECT</button>
+              </article>
+            <!-- </article> -->
+            <!-- <article class="row"> -->
+              <article class="discription_people">
+                <p class="topic_user">Space in galaxy</p>
+                <p class="name_user">by May Valentine</p>
+              </article> <!-- discription_people -->
+            <!-- </article> -->
+
+          </article> <!-- wrap_box_people 2 -->
+          <article class="col-xs-6 col-sm-4 col-md-3 wrap_box_people">
+            <!-- <article class="row"> -->
+              <article class="image_people">
+                <img src="image_people/03.jpg" alt="Ultraline 11.3">
+                  <button type="button">VIEW PROJECT</button>
+              </article>
+            <!-- </article> -->
+            <!-- <article class="row"> -->
+              <article class="discription_people">
+                <p class="topic_user">Space in galaxy</p>
+                <p class="name_user">by May Valentine</p>
+              </article> <!-- discription_people -->
+            <!-- </article> -->
+
+          </article> <!-- wrap_box_people 3 -->
+          <article class="col-xs-6 col-sm-4 col-md-3 wrap_box_people">
+            <!-- <article class="row"> -->
+              <article class="image_people">
+                <img src="image_people/04.jpg" alt="Ultraline 11.3">
+                  <button type="button">VIEW PROJECT</button>
+              </article>
+            <!-- </article> -->
+            <!-- <article class="row"> -->
+              <article class="discription_people">
+                <p class="topic_user">Space in galaxy</p>
+                <p class="name_user">by May Valentine</p>
+              </article> <!-- discription_people -->
+            <!-- </article> -->
+
+          </article> <!-- wrap_box_people 4 -->
+          <article class="col-xs-6 col-sm-4 col-md-3 wrap_box_people">
+            <!-- <article class="row"> -->
+              <article class="image_people">
+                <img src="image_people/05.jpg" alt="Ultraline 11.3">
+                  <button type="button">VIEW PROJECT</button>
+              </article>
+            <!-- </article> -->
+            <!-- <article class="row"> -->
+              <article class="discription_people">
+                <p class="topic_user">Space in galaxy</p>
+                <p class="name_user">by May Valentine</p>
+              </article> <!-- discription_people -->
+            <!-- </article> -->
+
+          </article> <!-- wrap_box_people 5 -->
+          <article class="col-xs-6 col-sm-4 col-md-3 wrap_box_people">
+            <!-- <article class="row"> -->
+              <article class="image_people">
+                <img src="image_people/06.jpg" alt="Ultraline 11.3">
+                  <button type="button">VIEW PROJECT</button>
+              </article>
+            <!-- </article> -->
+            <!-- <article class="row"> -->
+              <article class="discription_people">
+                <p class="topic_user">Space in galaxy</p>
+                <p class="name_user">by May Valentine</p>
+              </article> <!-- discription_people -->
+            <!-- </article> -->
+
+          </article> <!-- wrap_box_people 6 -->
+          <article class="col-xs-6 col-sm-4 col-md-3 wrap_box_people">
+            <!-- <article class="row"> -->
+              <article class="image_people">
+                <img src="image_people/07.jpg" alt="Ultraline 11.3">
+                  <button type="button">VIEW PROJECT</button>
+              </article>
+            <!-- </article> -->
+            <!-- <article class="row"> -->
+              <article class="discription_people">
+                <p class="topic_user">Space in galaxy</p>
+                <p class="name_user">by May Valentine</p>
+              </article> <!-- discription_people -->
+            <!-- </article> -->
+
+          </article> <!-- wrap_box_people 7 -->
+          <article class="col-xs-6 col-sm-4 col-md-3 wrap_box_people">
+            <!-- <article class="row"> -->
+              <article class="image_people">
+                <img src="image_people/08.jpg" alt="Ultraline 11.3">
+                  <button type="button">VIEW PROJECT</button>
+              </article>
+            <!-- </article> -->
+            <!-- <article class="row"> -->
+              <article class="discription_people">
+                <p class="topic_user">Space in galaxy</p>
+                <p class="name_user">by May Valentine</p>
+              </article> <!-- discription_people -->
+            <!-- </article> -->
+
+          </article> <!-- wrap_box_people 8 -->
+          <article class="col-xs-6 col-sm-4 col-md-3 wrap_box_people">
+            <!-- <article class="row"> -->
+              <article class="image_people">
+                <img src="image_people/09.jpg" alt="Ultraline 11.3">
+                  <button type="button">VIEW PROJECT</button>
+              </article>
+            <!-- </article> -->
+            <!-- <article class="row"> -->
+              <article class="discription_people">
+                <p class="topic_user">Space in galaxy</p>
+                <p class="name_user">by May Valentine</p>
+              </article> <!-- discription_people -->
+            <!-- </article> -->
+
+          </article> <!-- wrap_box_people 9 -->
+
+        </section> <!-- wrap_people -->
+      </section> <!-- row -->
+
+      <!-- footer -->
+      <footer class="row">
+        <section class="wrap_footer">
+          <article class="col-xs-12 wrap_r_footer">
+            <article class="col-xs-3 col-sm-2 col-md-1 wrap_footer_l">
+              <button type="button" name="button"><img src="image_web/btn_hamberger.png"></button>
+            </article> <!-- wrap_footer_l -->
+            <article class="col-xs-9 col-sm-10 col-md-11 wrap_footer_r">
+              <p>HOME > CATEGORY > APPLICATION IOS</p>
+              <article class="wrap_topic_footer">
+                <span class="topic_footer">Application IOS</span>
+              </article>
+              <article class="wrap_user_footer">
+                <span class="user_footer">12 CREATOR</span>
+              </article> <!-- wrap_user_footer -->
+            </article> <!-- wrap_footer_r -->
+          </article> <!-- wrap_r_footer -->
+        </section> <!-- wrap_footer -->
+      </footer>
+    </section> <!-- container-fluid -->
+
+
+  </body>
+</html>
